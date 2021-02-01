c87b47aad0df0bb89dcc60c13e12b2e93bf088d3
Kyle <kyleferry@icloud.com>
2021-02-01 23:59:27 +0000

    Added LaunchPad component

---
+++ src/components/LaunchPad.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const LaunchPad = (launchLocation) => {
+
+    const [LaunchPadItems, setLaunchPadItems] = useState([])
+    const [isLoading, setisLoading] = useState(true)
+    
+    useEffect(() => {
+      const fetchItems = async () => {
+        const result = await axios.get(`https://api.spacexdata.com/v4/launchpads/${launchLocation.launchLocation}`);
+        setLaunchPadItems(result.data)
+        setisLoading(false)
+      }
+      fetchItems()
+    },[])
+
+    console.log(launchLocation)
+
+    return (
+        <div>
+            <ul>
+                <li>Location: {LaunchPadItems.full_name}</li>
+            </ul>
+
+        </div>
+    )
+};
+
+export default LaunchPad
--- src/components/NextLaunchMain.js
+++ src/components/NextLaunchMain.js
@@ -1,6 +1,7 @@
 import falc9 from '../img/falcon-9-spacex-falcon-9-png-414_5414.png'
 import NextLaunchCountdown from '../components/NextLaunchCountdown'
 import Payload from '../components/Payload'
+import LaunchPad from '../components/LaunchPad'
 
 const NextLaunchMain = ({ isLoading, nextLaunchItems}) => {
     return isLoading ? (<h1>Loading...</h1>) : (<section className='card'>
@@ -8,6 +9,7 @@ const NextLaunchMain = ({ isLoading, nextLaunchItems}) => {
                 <h1>{nextLaunchItems.name}</h1>
                 <NextLaunchCountdown time={nextLaunchItems.date_utc}/>
                 <Payload payloadId={nextLaunchItems.payloads} />
+                <LaunchPad launchLocation={nextLaunchItems.launchpad} />
                 <img src={nextLaunchItems.links.patch.small} alt="Mission Badge"/>
                 <img src={falc9} alt="Falcon 9" className=''/>
             </div>
